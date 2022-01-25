37f134a3eb0f97e0bf7c29dfbdfd831fd6d6976d
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2022-01-25 19:27:01 +0530

    USDT-INR Trading Bot

--- demo.py
+++ demo.py
@@ -29,13 +29,18 @@ driver.maximize_window()
 driver.get(url) # launches the broswer and open url
 time.sleep(5) # very important everything to load before exicuting commands // safe at 5
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LOGIN ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 def login():
     driver.find_element(By.XPATH, "//span[normalize-space()='Login']").click()
     driver.find_element(By.XPATH, "//input[@id='mat-input-4']").send_keys("abhijayrajvansh01@gmail.com")
     driver.find_element(By.XPATH, "//input[@id='mat-input-5']").send_keys("@BJ@crypto!2711")
     driver.find_element(By.XPATH, "//button[@type='submit']").click()
     time.sleep(60)
-    print("Successfully Logged In!")
+    print("Successfully Logged In To CoinDCX Acc!")
+    print()
+
+
 
 
 
@@ -48,6 +53,7 @@ global bot_money
 global initial_allowed_money_to_bot
 global total_profit
 global elevation_amount
+global flag
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 global initial_bal
 global available_bal
@@ -64,8 +70,8 @@ total_profit = 0
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~BIDING~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Sensitive end points to observer fluctuatios: RESET at 00.00 and 100.00
-safe_low_BID = 81.42
-safe_high_BID = 81.43
+safe_low_BID = 00.00
+safe_high_BID = 100.00
 
 elevation_amount = 1 # 0.1 value so huge fluctuation
 
@@ -74,6 +80,7 @@ elevation_amount = 1 # 0.1 value so huge fluctuation
 bot_money = 100.00 # Bot allowed money to buy worth in rupees
 initial_allowed_money_to_bot = bot_money
 selling_usdt_worth_rupees = bot_money + ((bot_money * 1.2) / 100) # calculation required for 1.2% increase
+flag = 0
 
 
 
@@ -100,7 +107,7 @@ def BuyUSDT():
     global bot_money
     # Buy()
     print()
-    print("######################   ⬇   USDT Value Dropped Below Low Margin   ⬇  ######################")
+    print("######################    ⬇   USDT Value Dropped Below Low Margin   ⬇   ######################")
     print("********************** | Buying USDT worth of Rs." + str(bot_money) + " - Checkout done | **********************")
     print()
     print("Updated Low Margin Value : " + str(safe_low_BID) + " USDT")
@@ -146,7 +153,7 @@ def sell():
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ COMPARATOR ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def comparision(): # resolve between comparator of curr value and safe value points - done
-
+    global flag
     # Conversion
     temp_format_usdt_price = ""
     for char in curr_usdt_price:
@@ -158,17 +165,25 @@ def comparision(): # resolve between comparator of curr value and safe value poi
 
     if usdt_price <= safe_low_BID and bot_money != 0:
         BuyUSDT()
+        flag = 1
 
     elif usdt_price >= safe_high_BID and bot_money == 0:
         SellUSDT()
+        flag = 0
 
     else:
-        print("Incoming Orders: Buying At: " + str(safe_low_BID) + " | Selling At: " + str(safe_high_BID))
+        if flag == 0:
+            print("Incoming Orders: Buying At: " + str(safe_low_BID))
+        else:
+            print("Incoming Orders: Selling At: " + str(safe_high_BID))
 
 
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ DEBUGGER ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#login()
+
+
 
 def debug():
     time.sleep(1)
